9a1e2293d0f841d989cbb39a4218fa9170b94ad4
Alex <89089201593@mail.ru>
2021-06-16 12:26:19 +0500

    tabs added

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -40,6 +40,7 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    // showModalByTime('.popup', 100000);
     
 };
 
